d2da335a4a16593e520ba90069a065cbfc155977
Amber <ambersibel@yahoo.com>
2019-02-23 18:44:20 -0500

    hw04

--- inclass3_5/src/inclass3_5/Test.java
+++ inclass3_5/src/inclass3_5/Test.java
@@ -5,21 +5,13 @@ import java.util.*;
 public class Test{
   public static void main(String[] args){
     Scanner scan = new Scanner(System.in);
-    int num;
 
-    System.out.print("Enter a number: ");
-    num = scan.nextInt();
-
-    // Check if a number is even or odd
-    switch(num % 2){
-      case 0:
-        System.out.println("Num is odd!");
-        break;
-      case 1:
-        System.out.println("Num is even!");
-        break;
-    } // End switch-case
-
+    int a = 5;
+    double b = 10;
+    if(a > b);
+    {
+      System.out.println(a + " is greater than " + b);
+    }
   } // End main
 } // End class
 
